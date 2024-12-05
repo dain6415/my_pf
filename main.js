22e634b1972d1188c07643b7769b6ca270ea0b5e
dain6415 <ekdlscjs4@naver.com>
2024-12-05 20:21:07 +0900

    132

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // import {menu} from "./src/js/menu.js";
 // import {link} from "./src/js/link.js";
 import { header } from "./src/js/header.js";
-import { about } from "./src/js/about.js";
+// import { about } from "./src/js/about.js";
 import { intro } from "./src/js/intro.js";
 import { project } from "./src/js/project.js";
 // import { bgColor } from "./src/js/bg_color.js";
@@ -9,7 +9,7 @@ import { project } from "./src/js/project.js";
 window.addEventListener("load", function () {
   // menu()
   // link()
-  about();
+  // about();
   header();
   intro();
   project();
