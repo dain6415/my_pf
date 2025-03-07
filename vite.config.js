913de5589e41c80143e98878fc7b64f8b625e64e
dain6415 <ekdlscjs4@naver.com>
2025-03-07 20:31:05 +0900

    sdf

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  build:{
-    outDir:'dist'
+  root: './',  // 루트 디렉토리를 명시적으로 지정
+  build: {
+    outDir: 'dist',  // 빌드 결과물이 'dist' 폴더에 출력되도록
+    assetsDir: 'assets',
   }
 })
