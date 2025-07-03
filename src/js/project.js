5d9e09dd7e16b2f5171a2bd62f01faadbc234a78
dain6415 <ekdlscjs4@naver.com>
2025-07-04 02:33:16 +0900

    pro

--- src/js/project.js
+++ src/js/project.js
@@ -1,24 +1,41 @@
 export function project() {
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
+  new Swiper(".mySwiper", {
+    slidesPerView: 1,
     spaceBetween: 20,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-        spaceBetween: 10,
-      },
-      701: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      1300: {
-        slidesPerView: 3,
-        spaceBetween: 20,
-      },
-    },
+  });
+
+  let mobileMood = window.innerWidth <= 700;
+
+  window.addEventListener("resize", () => {
+    let noMobile = window.innerWidth <= 700;
+
+    if (noMobile !== mobileMood) {
+      mobileMood = noMobile;
+
+      const swiperSlide = document.querySelectorAll(".swiper-slide");
+
+      swiperSlide.forEach((slide) => {
+        const img = slide.querySelector('.img_box img');
+        if(!img)return;
+
+        let src = img.getAttribute('src');
+
+        if (noMobile) {
+          // 모바일일 때 _mini 붙임
+          if (!src.includes("_mini")) {
+            const newSrc = src.replace(/(\.\w+)$/, "_mini$1");
+            img.setAttribute("src", newSrc);
+          }
+        } else {
+          // 데스크탑일 때 _mini 제거
+          const newSrc = src.replace("_mini", "");
+          img.setAttribute("src", newSrc);
+        }
+      });
+    }
   });
 }
